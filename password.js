9a130e3d108e1177afc0c14b1f0a889a94e6b35b
Enis <enis.mermer.simplon@gmail.com>
2023-01-20 10:02:19 +0100

    ok

--- password.js
+++ password.js
@@ -8,23 +8,42 @@
 const chars = "abcdefghijklmnopqrstuvwxyz";
 const charsUpper = chars.toUpperCase();
 const numbers = "0123456789";
-const charsSpecial = ",;:!/.?%*&";
-
+const charsSpecial = ",;:!/.?%*@";
 const all = [chars, charsUpper, numbers, charsSpecial];
-console.log(all);
-
-// récupérer 8 caractères de manière aléatoire
 let password = "";
 
-function charsRandom(all, lengthPassword) {
-    for (let i = 1; i <= lengthPassword; i++) {
-        const randomNumber = Math.floor(
-            Math.random() * (all[0].length)
-        );
-    password += all[0][randomNumber];
-}
-console.log(password);
-}
-charsRandom(all, 8);
-// afficher le mot de passe
+// récupérer 8 caractères de manière aléatoire
 
+password += chars[Math.floor(Math.random() * chars.length)]
+password += charsUpper[Math.floor(Math.random() * charsUpper.length)]
+password += numbers[Math.floor(Math.random() * numbers.length)]
+password += charsSpecial[Math.floor(Math.random() * charsSpecial.length)]
+
+
+function charsRandom(all, lengthPassword) {
+    for (let i = password.length ; i <= lengthPassword - 1; i++) {
+        let groupRandom = all[Math.floor(Math.random() * all.length)]
+        let randomChar = groupRandom[Math.floor(Math.random() * groupRandom.length)]
+        password += randomChar
+}
+    return password
+}
+
+// Mélanger le mot de passe de façon aléatoire
+function shuffle (password) {
+    let passwordTable = password.split("")
+    let stringLength = passwordTable.length;
+
+    for(let i = 1; i <= stringLength - 1; i++) {
+        let random = Math.floor(Math.random() * i );
+        let newShuffleChar = passwordTable[i];
+        passwordTable[i] = passwordTable[random];
+        passwordTable[random] = newShuffleChar;
+    }
+    return passwordTable.join("");
+}
+
+
+// afficher le mot de passe
+console.log(charsRandom(all, 9))
+console.log(shuffle(charsRandom(all, 9)));
